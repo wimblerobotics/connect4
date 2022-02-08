58b8e9d7aced6fc5649316d1ca83599e98bce717
Michael Wimble <mike@wimblerobotics.com>
2022-02-07 22:19:49 -0800

    Improvements. Missing game logic still.

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ The number is in the range of 1 to 7, inclusive. If a column is full, the column
 to indicate that it will not accept an attempt to drop another marker there.
 
 The game starts with RED as the player. Currently, after you drop a marker, the game flips to 
-the other player (e.g., YELLOW).
+the other player (e.g., YELLOW). You can see who is playing by a small colored disk in the upper left.
+
+You can force which color plays next with the keys 'r' or 'y'.
 
 There is no real game logic yet.
 
--- connect4.py
+++ connect4.py
@@ -15,7 +15,7 @@ PLAYER2 = 2
 SQUARE_SIZE = 100
 
 class square:
-    def __init__(self, surface, x, y):
+    def __init__(self, surface, col, row):
         global SQUARE_SIZE, CELL_EMPTY, PLAYER1
         self._RED = (220, 0, 0)
         self._BLACK = (0, 0, 0)
@@ -24,11 +24,12 @@ class square:
         self._BACKGROUND = (54, 69, 212)
         self._YELLOW = (240, 240, 0)
         self._color = self._BLACK
-        self._x = x
-        self._y = y
+        self._col = col
+        self._row = row
         self._diameter = SQUARE_SIZE
         self._surface = surface
         self._occupied_by = CELL_EMPTY
+        self._winner = False
     
     def setOccupancy(self, occupied_by):
         self._occupied_by = occupied_by
@@ -37,15 +38,15 @@ class square:
         global HEADER_HEIGHT, CELL_RED, CELL_YELLOW, CELL_EMPTY
         radius = self._diameter / 2
         pygame.draw.rect(self._surface, self._BACKGROUND, [
-                         self._x * self._diameter, (self._y * self._diameter) + HEADER_HEIGHT, self._diameter, self._diameter])
-        pygame.draw.line(self._surface, self._BLACK, (self._x * self._diameter, (self._y * self._diameter) + HEADER_HEIGHT), 
-                                                      ((self._x + 1) * self._diameter, (self._y * self._diameter) + HEADER_HEIGHT), 2)
-        pygame.draw.line(self._surface, self._BLACK, ((self._x + 1) * self._diameter, (self._y * self._diameter) + HEADER_HEIGHT), 
-                                                      ((self._x + 1) * self._diameter, ((self._y + 1) * self._diameter) + HEADER_HEIGHT), 2)
-        pygame.draw.line(self._surface, self._BLACK, ((self._x + 1) * self._diameter, ((self._y + 1) * self._diameter) + HEADER_HEIGHT), 
-                                                      (self._x * self._diameter, ((self._y + 1) * self._diameter) + HEADER_HEIGHT), 2)
-        pygame.draw.line(self._surface, self._BLACK, (self._x * self._diameter, ((self._y + 1) * self._diameter) + HEADER_HEIGHT), 
-                                                      (self._x * self._diameter, (self._y * self._diameter) + HEADER_HEIGHT), 2)
+                         self._col * self._diameter, (self._row * self._diameter) + HEADER_HEIGHT, self._diameter, self._diameter])
+        pygame.draw.line(self._surface, self._BLACK, (self._col * self._diameter, (self._row * self._diameter) + HEADER_HEIGHT), 
+                                                      ((self._col + 1) * self._diameter, (self._row * self._diameter) + HEADER_HEIGHT), 2)
+        pygame.draw.line(self._surface, self._BLACK, ((self._col + 1) * self._diameter, (self._row * self._diameter) + HEADER_HEIGHT), 
+                                                      ((self._col + 1) * self._diameter, ((self._row + 1) * self._diameter) + HEADER_HEIGHT), 2)
+        pygame.draw.line(self._surface, self._BLACK, ((self._col + 1) * self._diameter, ((self._row + 1) * self._diameter) + HEADER_HEIGHT), 
+                                                      (self._col * self._diameter, ((self._row + 1) * self._diameter) + HEADER_HEIGHT), 2)
+        pygame.draw.line(self._surface, self._BLACK, (self._col * self._diameter, ((self._row + 1) * self._diameter) + HEADER_HEIGHT), 
+                                                      (self._col * self._diameter, (self._row * self._diameter) + HEADER_HEIGHT), 2)
         color = self._BLACK
         if self._occupied_by == CELL_RED:
             color = self._RED
@@ -53,20 +54,19 @@ class square:
             color = self._YELLOW
 
         pygame.draw.circle(self._surface, color, 
-            (self._x * self._diameter + radius, (self._y * self._diameter + radius) + HEADER_HEIGHT), (self._diameter / 2) - 12, 0)
+            (self._col * self._diameter + radius, (self._row * self._diameter + radius) + HEADER_HEIGHT), (self._diameter / 2) - 12, 0)
         
         if (color != self._BLACK):
             pygame.draw.circle(self._surface, self._BLACK, 
-                (self._x * self._diameter + radius, (self._y * self._diameter + radius) + HEADER_HEIGHT), (self._diameter / 2) - 15, 2)
+                (self._col * self._diameter + radius, (self._row * self._diameter + radius) + HEADER_HEIGHT), (self._diameter / 2) - 15, 2)
             pygame.draw.circle(self._surface, self._BLACK, 
-                (self._x * self._diameter + radius, (self._y * self._diameter + radius) + HEADER_HEIGHT), (self._diameter / 2) - 20, 1)
-            if ((self._x % 4) == 0):
+                (self._col * self._diameter + radius, (self._row * self._diameter + radius) + HEADER_HEIGHT), (self._diameter / 2) - 20, 1)
+            if (self._winner):
                 pygame.draw.circle(self._surface, self._WIN_LINE, 
-                    (self._x * self._diameter + radius, (self._y * self._diameter + radius) + HEADER_HEIGHT), (self._diameter / 2) - 9, 5)
-
-        # pygame.display.flip()
-        foo = 1
-
+                    (self._col * self._diameter + radius, (self._row * self._diameter + radius) + HEADER_HEIGHT), (self._diameter / 2) - 9, 5)
+            else:
+                pygame.draw.circle(self._surface, color,
+                    (self._col * self._diameter + radius, (self._row * self._diameter + radius) + HEADER_HEIGHT), (self._diameter / 2) - 9, 5)
 
 class Connect4Game:
     def __init__(self):
@@ -84,66 +84,95 @@ class Connect4Game:
         self._rows = 6
         self._cols = 7
 
-        # _squares = [[square()] * self._cols for _ in range(self._rows)]
         self._squares = []
-        for y in range(self._rows):
-            row = []
-            for x in range(self._cols):
-                row.append(square(self._display_surf, x, y))
-            self._squares.append(row)
+        for row in range(self._rows):
+            row_of_squares = []
+            for col in range(self._cols):
+                row_of_squares.append(square(self._display_surf, col, row))
+            self._squares.append(row_of_squares)
 
         self._running = True
 
     def dropColumn(self, occupancy, column):
         global CELL_EMPTY
         lastRow = -1
-        y = 0
-        while (y < self._rows) and (self._squares[y][column]._occupied_by == CELL_EMPTY):
-            lastRow = y
-            y = y + 1
+        row = 0
+        while (row < self._rows) and (self._squares[row][column]._occupied_by == CELL_EMPTY):
+            lastRow = row
+            row = row + 1
         
         if (lastRow == -1):
             return False
         else:
             self._squares[lastRow][column]._occupied_by = occupancy
+            self._squares[lastRow][column].render()
             return True
 
-    def next_cell(self, x, y, direction):
+    def next_cell(self, col, row, direction):
         if direction == 'up':
-            new_x = x + 0
-            new_y = y - 1
+            new_col = col + 0
+            new_row = row - 1
         elif direction == 'up_right':
-            new_x = x + 1
-            new_y = y - 1
+            new_col = col + 1
+            new_row = row - 1
         elif direction == 'right':
-            new_x = x + 1
-            new_y = y + 0
+            new_col = col + 1
+            new_row = row + 0
         elif direction == 'down_right':
-            new_x = x + 1
-            new_y = y + 1
+            new_col = col + 1
+            new_row = row + 1
         elif direction == 'down':
-            new_x = x + 0
-            new_y = y + 1
+            new_col = col + 0
+            new_row = row + 1
         elif direction == 'down_left':
-            new_x = x - 1
-            new_y = y + 1
+            new_col = col - 1
+            new_row = row + 1
         elif direction == 'left':
-            new_x = x - 1
-            new_y = y - 0
+            new_col = col - 1
+            new_row = row - 0
         elif direction == 'up_left':
-            new_x = x - 1
-            new_y = y - 1
+            new_col = col - 1
+            new_row = row - 1
         else:
-            new_x = -1
-            new_y = -1
+            new_col = -1
+            new_row = -1
         
-        if (new_x < 0) or (new_x >= self._cols) or (new_y < 0) or (new_y >= self._rows):
+        if (new_col < 0) or (new_col >= self._cols) or (new_row < 0) or (new_row >= self._rows):
             return False
         else:
-            return (new_x, new_y)
+            return (new_col, new_row)
 
-    # def is_4_in_a_row_starting_at(self, x, y):
+    def find_run_in_direction_starting_at(self, col, row, direction):
+        global CELL_EMPTY
+        if self._squares[row][col]._occupied_by == CELL_EMPTY:
+            return 0
+        else:
+            current_occupant = self._squares[row][col]._occupied_by
 
+        new_col = col
+        new_y = row
+        run_length = 0
+        for i in range(self._cols):
+            try:
+                if self._squares[new_y][new_col]._occupied_by == current_occupant:
+                    run_length += 1
+                    (new_col, new_y) = self.next_cell(new_col, new_y, direction)
+                else:
+                    break
+            except:
+                break
+        
+        return run_length
+
+    def game_won(self):
+        global CELL_EMPTY
+        for dir in ['up', 'up_right', 'right', 'down_right', 'down', 'down_left', 'left', 'up_left']:
+            for row in range(self._rows):
+                for col in range(self._cols):
+                    if self.find_run_in_direction_starting_at(col, row, dir) >= 4:
+                        return (row, col, dir, self._squares[row][col]._occupied_by)
+
+        return (-1, -1, 'up', CELL_EMPTY)
     
     def is_column_filled(self, column):
         global CELL_EMPTY
@@ -157,12 +186,13 @@ class Connect4Game:
             self._current_player = PLAYER1
 
     def on_event(self, event):
-        global CELL_RED, CELL_YELLOW, PLAYER1
+        global CELL_EMPTY, CELL_RED, CELL_YELLOW, PLAYER1
         if event.type == pygame.QUIT:
             self._running = False
         elif event.type == pygame.KEYDOWN:
             success = False
             current_player_cell = CELL_RED if self._current_player == PLAYER1 else CELL_YELLOW
+ 
             if event.key == pygame.K_1:
                 success = self.dropColumn(current_player_cell, 0)
             elif event.key == pygame.K_2:
@@ -177,29 +207,54 @@ class Connect4Game:
                 success = self.dropColumn(current_player_cell, 5)
             elif event.key == pygame.K_7:
                 success = self.dropColumn(current_player_cell, 6)
+            elif event.key == pygame.K_r:
+                self._current_player = PLAYER1
+            elif event.key == pygame.K_y:
+                self._current_player = PLAYER2
             
             if success:
                 self.swap_player()
+                # print("Run length[0,5]: %d", self.find_run_in_direction_starting_at(0, 0, 'down_right'))
+
+                # If winner found, mark winning squares.
+                (row, col, dir, winner) = self.game_won()
+                if winner != CELL_EMPTY:
+                    print("Game won by %s" % "RED" if winner == CELL_RED else "YELLOW")
+                    starting_square = self._squares[row][col]
+                    radius = starting_square._diameter / 2
+                    for counter in range(4):
+                        self._squares[row][col]._winner = True
+                        try:
+                            (c, r) = self.next_cell(col, row, dir)
+                            row = r
+                            col = c
+                        except:
+                            print("whoops")
 
     def on_loop(self):
         pass
 
     def on_render(self):
-        global BACKGROUND_COLOR, COLUMN_FONT_SIZE, HEADER_HEIGHT, SQUARE_SIZE
-        for y in range(self._rows):
-            for x in range(self._cols):
-                self._squares[y][x].render()
+        global BACKGROUND_COLOR, CELL_RED, COLUMN_FONT_SIZE, HEADER_HEIGHT, SQUARE_SIZE
 
-        pygame.font.init()
+        # Draw circle showing color of current player.
+        player_color_radius = 10
+        current_player_cell = CELL_RED if self._current_player == PLAYER1 else CELL_YELLOW
+        color = 'red' if current_player_cell == CELL_RED else 'yellow'
+        pygame.draw.circle(self._display_surf, color, 
+            (0 + player_color_radius, 0 + player_color_radius), player_color_radius, 0)
+
+        # Render squares.
+        for row in range(self._rows):
+            for col in range(self._cols):
+                self._squares[row][col].render()
+
+        # Show column headers.
         font = pygame.font.Font("arial.ttf", COLUMN_FONT_SIZE)
-        # col_numbers = font.render("1 2 3 4 5 6 7", True, (0,255,255))
-        # self._display_surf.blit(col_numbers, (40,90))
 
+        (row, col, dir, winner) = self.game_won()
         for col_number in range(0, 7):
-            # col_text = font.render("hello there", True, (0, 255, 255))
-            # col_text = font.render("1 2 3 4 5 6 7", True, (0,255,255))
-            # self._display_surf.blit(col_text, (col_number * 50, 50))
-            if self.is_column_filled(col_number):
+            if self.is_column_filled(col_number) or (winner != CELL_EMPTY):
                 column_number_color = BACKGROUND_COLOR
             else:
                 column_number_color = 'white'
@@ -217,19 +272,7 @@ class Connect4Game:
         if self.on_init() == False:
             self._running = False
         
-        # elem = "[{x:1d},{y:1d}] "
-        # for dir in ['up', 'up_right', 'right', 'down_right', 'down', 'down_left', 'left', 'up_left']:
-        #     print("\n--- %s ---" % dir)
-        #     for y in range(6):
-        #         line = ""
-        #         for x in range(7):
-        #             next_cell = self.next_cell(x, y, dir)
-        #             if (next_cell):
-        #                 line += elem.format(x = next_cell[0], y = next_cell[1])
-        #             else:
-        #                 line += " -,-  "
-        #         print(line)
-
+        pygame.font.init()
         while(self._running):
             for event in pygame.event.get():
                 self.on_event(event)
